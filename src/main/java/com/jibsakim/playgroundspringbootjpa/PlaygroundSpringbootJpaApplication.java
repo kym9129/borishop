b187f199538a8ba51b7a23d11b96cedc4f7dab8d
kym9129 <kym9129@naver.com>
2022-05-13 00:19:32 +0900

    initial commit

---
+++ src/main/java/com/jibsakim/playgroundspringbootjpa/PlaygroundSpringbootJpaApplication.java
@@ -0,0 +1,13 @@
+package com.jibsakim.playgroundspringbootjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlaygroundSpringbootJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlaygroundSpringbootJpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jibsakim/playgroundspringbootjpa/PlaygroundSpringbootJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jibsakim.playgroundspringbootjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlaygroundSpringbootJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
