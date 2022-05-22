9fd4e60c9216ecb5b1ebbab3ca43d95c927f7612
kym9129 <kym9129@naver.com>
2022-05-23 01:10:28 +0900

    style: response 날짜포멧 변경

--- src/main/java/com/borishop/web/dto/product/ProductResponseDto.java
+++ src/main/java/com/borishop/web/dto/product/ProductResponseDto.java
@@ -2,6 +2,7 @@ package com.borishop.web.dto.product;
 
 import com.borishop.constant.ProductSellStatus;
 import com.borishop.domain.product.Product;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Getter;
 
 import java.time.LocalDateTime;
@@ -14,7 +15,9 @@ public class ProductResponseDto {
     private int stockNumber; // 재고수량
     private String productDetail; // 상품 상세 설명
     private ProductSellStatus productSellStatus; // 상품 판매상태
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")
     private LocalDateTime createdAt; // 등록 시간
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")
     private LocalDateTime updatedAt; // 수정 시간
 
     public ProductResponseDto(Product entity){
