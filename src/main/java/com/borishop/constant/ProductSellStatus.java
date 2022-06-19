5ddf8ae6950cd51e48d95e52fd8c245f89904a01
kym9129 <kym9129@naver.com>
2022-06-19 23:22:30 +0900

    Auditing추가, Querydsl 실습

--- src/main/java/com/borishop/constant/ProductSellStatus.java
+++ src/main/java/com/borishop/constant/ProductSellStatus.java
@@ -1,5 +1,5 @@
 package com.borishop.constant;
 
 public enum ProductSellStatus {
-    WAIT, SELL, SOLDOUT, CLOSE // 판매대기, 판매개시, 일시품절, 판매종료
+    WAIT, SELL, SOLD_OUT, CLOSE // 판매대기, 판매개시, 일시품절, 판매종료
 }
--- src/main/java/com/borishop/domain/product/ProductRepository.java
+++ src/main/java/com/borishop/domain/product/ProductRepository.java
@@ -2,11 +2,12 @@ package com.borishop.domain.product;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
-public interface ProductRepository extends JpaRepository<Product, Long> {
+public interface ProductRepository extends JpaRepository<Product, Long>, QuerydslPredicateExecutor<Product> {
 
     List<Product> findByProductName(String productName);
 
