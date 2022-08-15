348434525430d50825bdc4de437fe4e3a4b14083
kym9129 <kym9129@naver.com>
2022-08-15 22:56:06 +0900

    feat: Spring Security 로그인 구현

---
+++ src/main/java/com/borishop/config/auth/dto/SessionUser.java
@@ -0,0 +1,21 @@
+package com.borishop.config.auth.dto;
+
+import com.borishop.domain.user.User;
+import lombok.Getter;
+import lombok.ToString;
+
+import java.io.Serializable;
+
+@Getter
+@ToString
+public class SessionUser implements Serializable {
+    private String name;
+    private String email;
+    private String picture;
+
+    public SessionUser(User user){
+        this.name = user.getName();
+        this.email = user.getEmail();
+        this.picture = user.getPicture();
+    }
+}
