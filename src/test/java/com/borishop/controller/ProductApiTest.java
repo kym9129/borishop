6504413187313ab53b10b20dfcb7380d64e34fe3
kym9129 <kym9129@naver.com>
2022-10-04 00:48:42 +0900

    test: 상품 API 테스트 수정

--- src/test/java/com/borishop/controller/ProductApiTest.java
+++ src/test/java/com/borishop/controller/ProductApiTest.java
@@ -5,39 +5,56 @@ import com.borishop.domain.product.ProductRepository;
 import com.borishop.web.dto.product.ProductCreateRequestDto;
 import com.borishop.constant.ProductSellStatus;
 import com.borishop.web.dto.product.ProductUpdateRequestDto;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.Disabled;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.boot.web.server.LocalServerPort;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.*;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest(
         webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT
 )
 @ActiveProfiles("test")
-public class ProductIntegrationTest {
+public class ProductApiTest {
 
-    @LocalServerPort
     private int port;
 
     @Autowired
-    private TestRestTemplate restTemplate;
+    private ProductRepository productRepository;
 
     @Autowired
-    private ProductRepository productRepository;
+    private WebApplicationContext context;
+
+    private ObjectMapper objectMapper = new ObjectMapper();
+
+    @MockBean
+    JPAQueryFactory jpaQueryFactory;
+
+    private MockMvc mvc;
+
+    @BeforeEach
+    public void setup() {
+        mvc = MockMvcBuilders
+                .webAppContextSetup(context)
+                .apply(springSecurity())
+                .build();
+    }
 
     @AfterEach
     void tear_down() throws Exception {
@@ -46,7 +63,7 @@ public class ProductIntegrationTest {
 
     @Test
     @DisplayName("상품 등록")
-    @Disabled("일단 유닛테스트부터 하고")
+    @WithMockUser(roles="ADMIN")
     void product_add() throws Exception {
         // given
         ProductCreateRequestDto givenRequestDto = ProductCreateRequestDto.builder()
@@ -60,13 +77,10 @@ public class ProductIntegrationTest {
         String url = "http://localhost:"+port+"/api/product";
 
         // when
-        ResponseEntity<Long> responseEntity = restTemplate.postForEntity(url, givenRequestDto, Long.class);
-
-        // then
-        assertAll(
-                () -> assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK),
-                () -> assertThat(responseEntity.getBody()).isGreaterThan(0L)
-        );
+        mvc.perform(post(url)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(givenRequestDto))
+        ).andExpect(status().isOk());
 
         List<Product> all = productRepository.findAll();
         assertAll(
@@ -76,13 +90,12 @@ public class ProductIntegrationTest {
                 () -> assertThat(all.get(0).getDetail()).isEqualTo(givenRequestDto.getProductDetail()),
                 () -> assertThat(all.get(0).getSellStatus()).isEqualTo(givenRequestDto.getProductSellStatus())
         );
-
     }
 
     @Test
     @DisplayName("상품 수정 테스트")
-    @Disabled("일단 유닛테스트부터 하고")
-    void update(){
+    @WithMockUser(roles="ADMIN")
+    void update() throws Exception {
         // given
         Product saveProduct = productRepository.save(Product.builder()
                 .name("[무료배송] 보리의 깃털장난감")
@@ -103,16 +116,13 @@ public class ProductIntegrationTest {
 
         String url = "http://localhost:"+port+"/api/product/"+updateId;
 
-        HttpEntity<ProductUpdateRequestDto> requestEntity = new HttpEntity<>(givenRequestDto);
-
         // when
-        // PUT은 putForEntity가 없어서 exchange를 사용해서 보내나보다
-        ResponseEntity<Long> responseEntity = restTemplate.exchange(url, HttpMethod.PUT, requestEntity, Long.class);
+        mvc.perform(put(url)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(givenRequestDto))
+        ).andExpect(status().isOk());
 
         // then
-        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
-        assertThat(responseEntity.getBody()).isGreaterThan(0L);
-
         Product product = productRepository.findById(updateId).orElseThrow(null);
         assertAll(
                 () -> assertThat(product.getName()).isEqualTo(givenRequestDto.getProductName()),
@@ -121,8 +131,6 @@ public class ProductIntegrationTest {
                 () -> assertThat(product.getDetail()).isEqualTo(givenRequestDto.getProductDetail()),
                 () -> assertThat(product.getSellStatus()).isEqualTo(givenRequestDto.getProductSellStatus())
         );
-
-
     }
 
 }
